b8aae716f063eeff43f1a42cc7c16be36304a1b9
utkuzun <utkuzun50@gmail.com>
2022-08-06 13:48:34 +0300

    async errors handle

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,14 @@
 const Blog = require('../models/Blog')
 
-const getAll = async (req, res, next) => {
-  try {
-    const blogs = await Blog.find({})
-    res.json(blogs)
-  } catch (error) {
-    next(error)
-  }
+const getAll = async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
 }
 
-const create = async (req, res, next) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.status(201).json({ blog })
-  } catch (error) {
-    next(error)
-  }
+const create = async (req, res) => {
+  const blog = await Blog.create(req.body)
+  res.status(201).json({ blog })
+
 }
 
 module.exports = { getAll, create }
